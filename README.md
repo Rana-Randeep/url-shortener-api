a0e9b6f7d8adac1c1e28866102fc1b50481a4e6b
Rana-Randeep <ranarandeep305@gmail.com>
2026-05-07 15:25:31 +0530

    feat:Initialize project with health check and url entity

---
+++ README.md
@@ -0,0 +1,12 @@
+# URL Shortener API
+
+A URL shortening service built with Java and Spring Boot.
+
+## Tech Stack
+- Java 21
+- Spring Boot 4.0.6
+- MySQL
+- Maven
+
+## Status
+🚧 In Progress
---
+++ url-shortener-api/src/main/java/com/urlshortener/url_shortener_api/controller/HealthController.java
@@ -0,0 +1,17 @@
+package com.urlshortener.url_shortener_api.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
+
+@RestController
+@RequestMapping("/api")
+public class HealthController {
+
+    @GetMapping("/health")
+    public ResponseEntity<String> health() {
+        return ResponseEntity.ok("URL Shortener API is running");
+    }
+}
