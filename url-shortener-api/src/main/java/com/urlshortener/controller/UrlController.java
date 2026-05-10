daeae4585137b2223927a9290a1e06a1ac57a4ad
Rana-Randeep <ranarandeep305@gmail.com>
2026-05-10 23:26:09 +0530

    feat: add GET /{shortCode} redirect endpoint

--- url-shortener-api/src/main/java/com/urlshortener/controller/UrlController.java
+++ url-shortener-api/src/main/java/com/urlshortener/controller/UrlController.java
@@ -3,11 +3,11 @@ package com.urlshortener.controller;
 import com.urlshortener.dto.request.ShortenRequest;
 import com.urlshortener.dto.response.ShortenResponse;
 import com.urlshortener.service.UrlService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/api")
@@ -23,6 +23,15 @@ public class UrlController {
     public ResponseEntity<ShortenResponse> shortenUrl(@RequestBody ShortenRequest request) {
         ShortenResponse response = urlService.shortenUrl(request);
         return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/{shortCode}")
+    public ResponseEntity<Void> redirect(@PathVariable String shortCode) {
+        String originalUrl = urlService.getOriginalUrl(shortCode);
+
+        return ResponseEntity.status(HttpStatus.FOUND) //HttpStatus.FOUND = HTTP 302 = standard redirect response.
+                .location(URI.create(originalUrl))
+                .build();
     }
 
 }
